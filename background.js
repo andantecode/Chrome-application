a1d4d371bcce0a9b6d6a64d6e1abdb8e35c7a2ac
semo08 <110328964+semo08@users.noreply.github.com>
2025-11-03 12:57:10 +0900

    Add more images to the background selection

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
@@ -7,3 +7,4 @@ bgImage.src = `${chosenImage}`;
 
 
 document.body.appendChild(bgImage);
+
