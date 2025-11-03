718fa85a93f801f2bc474d4aceed2a2ebbe79fa1
semo08 <110328964+semo08@users.noreply.github.com>
2025-11-03 12:57:55 +0900

    Implement basic todo list functionality

---
+++ todo.js
@@ -0,0 +1,29 @@
+const todoForm = document.getElementById("todo-form");
+const todoInput = todoForm.querySelector("#todo-form input");
+const todoList = document.getElementById("todo-list");
+
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);
+}
+
+function handletodoSubmit(event) {
+    event.preventDefault();
+    const newTodo = todoInput.value;
+    todoInput.value = "";
+    paintTodo(newTodo);
+}
+
+todoForm.addEventListener("submit", handletodoSubmit);  
